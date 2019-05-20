a5c8dbfe23c936f539b7f5541cee943aaa7c4024
Aminul <Aminul@192.168.0.47>
2019-05-19 22:23:36 -0700

    reverting changes made

--- src/test/java/Pages/AutoTrader.java
+++ src/test/java/Pages/AutoTrader.java
@@ -17,30 +17,27 @@ import Library.Base;
 public class AutoTrader extends Base {
 
 	public void GoToWebSite() throws InterruptedException {
-		driver.get("https://www.costco.com/");
-		Thread.sleep(5000);
-		WebElement GroceryOption = driver.findElement(By.xpath("//a[@id='Home_Ancillary_0' and @data-flyout=\"Home_Ancillary_Popover_0\"]"));
-		GroceryOption.click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@id=\"postal-code-input\"]")).sendKeys("22191");
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@id=\"postal-code-submit\"]")).click();
-		Thread.sleep(5000);
+			driver.get("https://www.costco.com/");
+			Thread.sleep(5000);
+			WebElement GroceryOption = driver.findElement(By.xpath("//*[@id=\"Home_Ancillary_0\"]"));
+			GroceryOption.click();
+				Thread.sleep(5000);
+				driver.findElement(By.xpath("//*[@id=\"postal-code-input\"]")).sendKeys("22191");
+				driver.findElement(By.xpath("//*[@id=\"postal-code-submit\"]")).click();
+				Thread.sleep(5000);
 		driver.findElement(By.xpath("//*[@id=\"search-results\"]/div[6]/div/div/div[1]/div[2]/div[5]/a")).click();
 		Thread.sleep(5000);
-		WebElement PeanutButter = driver.findElement(By.xpath("//*[@id=\"value-5\"]"));
-		PeanutButter.clear();
-		PeanutButter.sendKeys("3");
-		driver.findElement(By.xpath("//*[@id=\"addbutton-5\"]")).click();
-		Thread.sleep(10000);
-		driver.findElement(By.xpath("//*[@id=\"cart-d\"]")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@id=\"cart-d\"]")).click();
-		Thread.sleep(5000);
-		String PB = driver.findElement(By.xpath("//*[@id=\"order-items-grocery\"]/div/div/div[2]/div[1]/a")).getText();
-		assertEquals(PB, "Kirkland Signature Organic Peanut Butter, 28 oz, 2-count");
-		driver.findElement(By.xpath("//*[@id=\"shopCartCheckoutSubmitButton\"]")).click();
-		
+		 WebElement PeanutButter = driver.findElement(By.xpath("//*[@id=\"value-5\"]"));
+		 PeanutButter.clear();
+		 PeanutButter.sendKeys("3");
+		 driver.findElement(By.xpath("//*[@id=\"addbutton-5\"]")).click();
+		 Thread.sleep(5000);
+		 driver.findElement(By.xpath("//*[@id=\"cart-d\"]")).click();
+		 Thread.sleep(5000);
+	String PB	= driver.findElement(By.xpath("//*[@id=\"order-items-grocery\"]/div/div/div[2]/div[1]/a")).getText();
+	assertEquals(PB, "Kirkland Signature Organic Peanut Butter, 28 oz, 2-count");
+	driver.findElement(By.xpath("//*[@id=\"shopCartCheckoutSubmitButton\"]")).click();
+
 	}
 //
 	@Test
